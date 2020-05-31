ce85a004f470003d3e99da9726a95fc176346ca2
Revin Tanri <tanrirevin@gmail.com>
2020-05-31 13:04:36 +0800

    Install the redux, react-redux (2nd)

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import reducer from './reducers'
+
+const store = createStore(reducer)
 
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>
+  ,document.getElementById('root')
 );
 
