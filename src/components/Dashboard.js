74b60842c6c98a2a18aeae3a4d511691f2d34f97
Revin Tanri <tanrirevin@gmail.com>
2020-06-09 23:06:25 +0800

    Set the answeredQuestion in the Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,30 +3,56 @@ import {connect} from 'react-redux'
 import Question from './Question'
 import MenuBar from './MenuBar'
 import Divider from './Divider'
+import {handleInitialQuestions} from '../actions/shared'
 
 class Dashboard extends Component {
+    componentDidMount(){
+        this.props.dispatch(handleInitialQuestions())
+    }
+
+
     render(){
-        console.log(this.props)
+        const {answeredQuestions, /*unansweredQuestions*/} = this.props
+        console.log('Answered Questions', answeredQuestions)
         return (
             <Fragment>
                 <MenuBar />
                 <h1 className='page-title'>Your Question Pool</h1>
                 <Divider />
-                <div className='question-form margin'>
+                {
+                    answeredQuestions ? 
+                    <div className='question-form margin'>
                     {this.props.questionIds.map((id) => (
                         <Question key={id} id={id}></Question>
                     ))}
-                </div>
+                    </div>
+                    : null 
+                }
             </Fragment>
         )
     }
 }
 
-function mapStateToProps({questions}) {
-    return {
-        questionIds:Object.keys(questions)
+function mapStateToProps({questions, authedUser, users }) {
+    const user = users[authedUser]
+
+    const answeredQuestions = Object.keys(questions).length !== 0
+        ? Object.keys(user.answers)
         .sort ((a,b) => questions[b].timestamp - questions[a].timestamp)
+        : []
+
+    const unansweredQuestions = Object.keys(questions).length !== 0
+        ? Object.keys(questions)
+        .filter(questionid => !answeredQuestions.includes(questionid))
+        .sort((a,b) => questions[b].timestamp -questions[a].timestamp)
+        : []
+    
+    return {
+        answeredQuestions, unansweredQuestions,
     }
+
 }
+
+
 
 export default connect(mapStateToProps)(Dashboard)
