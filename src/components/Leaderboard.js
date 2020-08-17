b6db4e6989b736976f96b6ed5cc5294db6d3de8b
Revin Tanri <tanrirevin@gmail.com>
2020-08-17 17:54:28 +0800

    Create Leaderboard page layout

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,13 +1,53 @@
 import React, {Component} from 'react'
 import {
-    Header
+    Header,
+    Image,
+    Table,
+    Label,
 } from 'semantic-ui-react'
 
 
 class Leaderboard extends Component{
     render(){
         return(
-            <Header> Hall of Fame </Header>
+            <div class="contentMargin">
+                <Table>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>Rank</Table.HeaderCell>
+                        <Table.HeaderCell>User</Table.HeaderCell>
+                        <Table.HeaderCell>Polls Created</Table.HeaderCell>
+                        <Table.HeaderCell>Polls Answered</Table.HeaderCell>
+                    </Table.Row>                    
+                </Table.Header>
+
+                <Table.Body>
+
+                    <Table.Row>
+                        <Table.Cell>1</Table.Cell>
+                        <Table.Cell>Name 1</Table.Cell>
+                        <Table.Cell>12</Table.Cell>
+                        <Table.Cell>91</Table.Cell>
+                    </Table.Row>
+
+                    <Table.Row>
+                        <Table.Cell>2</Table.Cell>
+                        <Table.Cell>Name 2</Table.Cell>
+                        <Table.Cell>34</Table.Cell>
+                        <Table.Cell>81</Table.Cell>
+                    </Table.Row>
+
+                    <Table.Row>
+                        <Table.Cell>3</Table.Cell>
+                        <Table.Cell>Name 3</Table.Cell>
+                        <Table.Cell>56</Table.Cell>
+                        <Table.Cell>71</Table.Cell>
+                    </Table.Row>
+
+                </Table.Body>
+
+            </Table>
+            </div>
         )
     }
 }
